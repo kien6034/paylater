b346b3eccb88de0cf80bc3e88c5f53ec799ae699
kien6034 <trinhtrungkientb99@gmail.com>
2023-12-27 00:47:00 +0700

    feat: can do cpi from paylater to nemo

--- programs/paylater/src/instructions/buy_first.rs
+++ programs/paylater/src/instructions/buy_first.rs
@@ -16,32 +16,32 @@ pub fn buy_first(
     amount_specified_is_input: bool,
     a_to_b: bool
 ) -> Result<(), ProgramError> {
-    // let cpi_program = ctx.accounts.whirlpool_program.to_account_info().clone();
-    // let cpi_accounts = Swap {
-    //     token_program: ctx.accounts.token_program.to_account_info(),
-    //     token_authority: ctx.accounts.user.to_account_info(),
-    //     whirlpool: ctx.accounts.whirlpool.to_account_info(),
-    //     token_owner_account_a: ctx.accounts.token_owner_account_a.to_account_info(),
-    //     token_vault_a: ctx.accounts.token_vault_a.to_account_info(),
-    //     token_owner_account_b: ctx.accounts.token_owner_account_b.to_account_info(),
-    //     token_vault_b: ctx.accounts.token_vault_b.to_account_info(),
-    //     tick_array_0: ctx.accounts.tick_array_0.to_account_info(),
-    //     tick_array_1: ctx.accounts.tick_array_1.to_account_info(),
-    //     tick_array_2: ctx.accounts.tick_array_2.to_account_info(),
-    //     oracle: ctx.accounts.oracle.to_account_info(),
-    // };
+    let cpi_program = ctx.accounts.whirlpool_program.to_account_info().clone();
+    let cpi_accounts = Swap {
+        token_program: ctx.accounts.token_program.to_account_info(),
+        token_authority: ctx.accounts.user.to_account_info(),
+        whirlpool: ctx.accounts.whirlpool.to_account_info(),
+        token_owner_account_a: ctx.accounts.token_owner_account_a.to_account_info(),
+        token_vault_a: ctx.accounts.token_vault_a.to_account_info(),
+        token_owner_account_b: ctx.accounts.token_owner_account_b.to_account_info(),
+        token_vault_b: ctx.accounts.token_vault_b.to_account_info(),
+        tick_array_0: ctx.accounts.tick_array_0.to_account_info(),
+        tick_array_1: ctx.accounts.tick_array_1.to_account_info(),
+        tick_array_2: ctx.accounts.tick_array_2.to_account_info(),
+        oracle: ctx.accounts.oracle.to_account_info(),
+    };
 
-    // let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+    let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
 
-    // // do cpi call
-    // whirlpool::cpi::swap(
-    //     cpi_ctx,
-    //     amount,
-    //     other_amount_threshold,
-    //     sqrt_price_limit,
-    //     amount_specified_is_input,
-    //     a_to_b
-    // )
+    // do cpi call
+    whirlpool::cpi::swap(
+        cpi_ctx,
+        amount,
+        other_amount_threshold,
+        sqrt_price_limit,
+        amount_specified_is_input,
+        a_to_b
+    );
     Ok(())
 }
 
@@ -55,31 +55,31 @@ pub struct BuyFirst<'info> {
 
     pub whirlpool_program: Program<'info, WhirlpoolProgram>,
 
-    // #[account(mut)]
-    // pub whirlpool: Box<Account<'info, Whirlpool>>,
+    #[account(mut)]
+    pub whirlpool: Box<Account<'info, Whirlpool>>,
 
-    // #[account(mut, constraint = token_owner_account_a.mint == whirlpool.token_mint_a)]
-    // pub token_owner_account_a: Box<Account<'info, TokenAccount>>,
-    // #[account(mut, address = whirlpool.token_vault_a)]
-    // pub token_vault_a: Box<Account<'info, TokenAccount>>,
+    #[account(mut, constraint = token_owner_account_a.mint == whirlpool.token_mint_a)]
+    pub token_owner_account_a: Box<Account<'info, TokenAccount>>,
+    #[account(mut, address = whirlpool.token_vault_a)]
+    pub token_vault_a: Box<Account<'info, TokenAccount>>,
 
-    // #[account(mut, constraint = token_owner_account_b.mint == whirlpool.token_mint_b)]
-    // pub token_owner_account_b: Box<Account<'info, TokenAccount>>,
-    // #[account(mut, address = whirlpool.token_vault_b)]
-    // pub token_vault_b: Box<Account<'info, TokenAccount>>,
+    #[account(mut, constraint = token_owner_account_b.mint == whirlpool.token_mint_b)]
+    pub token_owner_account_b: Box<Account<'info, TokenAccount>>,
+    #[account(mut, address = whirlpool.token_vault_b)]
+    pub token_vault_b: Box<Account<'info, TokenAccount>>,
 
-    // #[account(mut, has_one = whirlpool)]
-    // pub tick_array_0: AccountLoader<'info, TickArray>,
+    #[account(mut, has_one = whirlpool)]
+    pub tick_array_0: AccountLoader<'info, TickArray>,
 
-    // #[account(mut, has_one = whirlpool)]
-    // pub tick_array_1: AccountLoader<'info, TickArray>,
+    #[account(mut, has_one = whirlpool)]
+    pub tick_array_1: AccountLoader<'info, TickArray>,
 
-    // #[account(mut, has_one = whirlpool)]
-    // pub tick_array_2: AccountLoader<'info, TickArray>,
+    #[account(mut, has_one = whirlpool)]
+    pub tick_array_2: AccountLoader<'info, TickArray>,
 
     // #[account(seeds = [b"oracle", whirlpool.key().as_ref()], bump)]
-    // /// Oracle is currently unused and will be enabled on subsequent updates
-    // pub oracle: UncheckedAccount<'info>,
+    /// CHECK: This is not dangerous
+    pub oracle: UncheckedAccount<'info>,
 
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
